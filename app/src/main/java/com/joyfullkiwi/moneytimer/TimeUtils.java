eeeba3f632925c2c9eb3ee666b01a844dd1e7ed5
NikolayGst <nikonic95@gmail.com>
2018-04-14 19:37:56 +0300

    format time 2

--- app/src/main/java/com/joyfullkiwi/moneytimer/TimeUtils.java
+++ app/src/main/java/com/joyfullkiwi/moneytimer/TimeUtils.java
@@ -3,8 +3,8 @@ package com.joyfullkiwi.moneytimer;
 public class TimeUtils {
 
   public static String formatTime(long time) {
-    long hours = (time / 60 / 60 / 1000) % 24;
-    long minutes = (time / 60 / 1000) % 60;
+    long hours = (time / 60 / 60 / 100) % 24;
+    long minutes = (time / 60 / 100) % 60;
     long seconds = (time / 100) % 60;
     return String.format("%02d:%02d:%02d", hours, minutes, seconds);
   }
